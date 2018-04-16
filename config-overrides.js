f19539b830cd8bc0184a632e39fdc556c68a632b
youjie <youjie@hudongpai.com>
2018-04-16 14:39:26 +0800

    按需加载

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,26 @@
+const tsImportPluginFactory = require("ts-import-plugin");
+const { getLoader } = require("react-app-rewired");
+
 module.exports = function override(config, env) {
-  // do stuff with the webpack config...
+  const tsLoader = getLoader(
+    config.module.rules,
+    rule =>
+      rule.loader &&
+      typeof rule.loader === "string" &&
+      rule.loader.includes("ts-loader")
+  );
+
+  tsLoader.options = {
+    getCustomTransformers: () => ({
+      before: [
+        tsImportPluginFactory({
+          libraryName: "antd",
+          libraryDirectory: "es",
+          style: "css"
+        })
+      ]
+    })
+  };
+
   return config;
 };
